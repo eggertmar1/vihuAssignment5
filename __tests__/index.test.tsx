b83bffae3ec69584716870fab7e9616f9421c5b6
Eggert Eggertsson <eggerte19@ru.is>
2022-03-05 00:49:38 +0000

    All tests done

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,11 +1,74 @@
 import React from "react";
 import "@testing-library/jest-dom";
 import Home from "../pages/index";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import { render, fireEvent, waitFor, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { json } from "stream/consumers";
 
-// Good starting point: https://testing-library.com/docs/react-testing-library/example-intro
+const server = setupServer(
+    rest.get("/api/list", (req, res, ctx) => {
+        return res(ctx.status(200), ctx.json([{ id: "1", title: "Todo 1" }]));
+    }),
+    rest.post("/api/add", (req, res, ctx) => {
+        return res(ctx.status(200), ctx.json({ id: "2", title: "Todo 2" }));
+    }),
+    rest.delete("/api/remove", (req, res, ctx) => {
+        return res(ctx.status(200));
+    })
+);
 
-// TODO setup your mock api here
+// 3. Start the Service Worker.
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+// mock the api call
 
 describe("Tests", () => {
-  // TODO Add your react-testing-library tests here
+    // Write a test that asserts that loading is displayed when the response is not correct
+    test("Loading is displayed when the response is not correct", async () => {
+        server.use(
+            rest.get("/api/list", (req, res, ctx) => {
+                return res(ctx.json(null));
+            })
+        );
+        render(<Home />);
+        await waitFor(() =>
+            expect(screen.getByTestId("loading")).toBeInTheDocument()
+        );
+    });
+    // Write a test that asserts that a single item is in the list when the component is loaded
+    test("A single item is in the list when the component is loaded", async () => {
+        render(<Home />);
+        await waitFor(() =>
+            expect(screen.getAllByTestId("todo-item")).toHaveLength(1)
+        );
+    });
+    // Write a test that adds a new item to the list
+    test("Adds a new item to the list", async () => {
+        render(<Home />);
+        await waitFor(() =>
+            expect(screen.getAllByTestId("todo-item")).toHaveLength(1)
+        );
+        fireEvent.change(screen.getByTestId("todo-input"), {
+            target: { value: "Todo 2" },
+        });
+        fireEvent.submit(screen.getByTestId("todo-input"));
+        await waitFor(() =>
+            expect(screen.getAllByTestId("todo-item")).toHaveLength(2)
+        );
+    });
+    // Write a test that removes an item from the list
+    test("Removes an item from the list", async () => {
+        render(<Home />);
+        await waitFor(() =>
+            expect(screen.getAllByTestId("todo-item")).toHaveLength(1)
+        );
+        fireEvent.click(screen.getByTestId("todo-item"));
+        await waitFor(() =>
+            expect(screen.queryAllByTestId("todo-item")).toHaveLength(0)
+        );
+    });
 });
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,111 +3,105 @@ import styles from "../styles/Home.module.css";
 import { SetStateAction, useEffect, useState } from "react";
 import axios from "axios";
 import { Todo } from "../lib/todoStore";
-import Image from "next/image";
 
 export default function Home() {
-  const [todos, setTodos] = useState<Todo[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [todo, setTodo] = useState("");
+    const [todos, setTodos] = useState<Todo[]>([]);
+    const [todo, setTodo] = useState("");
 
-  useEffect(() => {
-    setLoading(true);
-    loadTodos();
-  }, []);
+    useEffect(() => {
+        loadTodos();
+    }, []);
 
-  let changeHandler = (event: {
-    target: { value: SetStateAction<string> };
-  }) => {
-    setTodo(event.target.value);
-  };
+    let changeHandler = (event: {
+        target: { value: SetStateAction<string> };
+    }) => {
+        setTodo(event.target.value);
+    };
 
-  let addTodo = async (event: { preventDefault: () => void }) => {
-    if (todo.length === 0) {
-      return;
+    let addTodo = async (event: { preventDefault: () => void }) => {
+        if (todo.length === 0) {
+            return;
+        }
+
+        setTodo("");
+        event.preventDefault();
+        const { data } = await axios.post("/api/add", { title: todo });
+
+        setTodos([...todos, data]);
+    };
+
+    let removeTodo = async (rtodo: Todo) => {
+        await axios.delete(`/api/remove?id=${rtodo.id}`);
+
+        const filteredData = todos.filter((d) => d.id !== rtodo.id);
+        setTodos(filteredData);
+    };
+
+    let loadTodos = async () => {
+        const { data } = await axios.get("/api/list");
+        setTodos(data);
+    };
+
+    if (!todos) {
+        return <div data-testid="loading">Loading...</div>;
     }
-    setLoading(true);
-    setTodo("");
-    event.preventDefault();
-    const { data } = await axios.post("/api/add", { title: todo });
-    setLoading(false);
-    setTodos([...todos, data]);
-  };
 
-  let removeTodo = async (rtodo: Todo) => {
-    setLoading(true);
-    await axios.delete(`/api/remove?id=${rtodo.id}`);
-    setLoading(false);
-    const filteredData = todos.filter((d) => d.id !== rtodo.id);
-    setTodos(filteredData);
-  };
+    return (
+        <div className={styles.container} data-testid="application">
+            <Head>
+                <title>A5 - TODO</title>
+                <link
+                    rel="icon"
+                    href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨‍🏫</text></svg>"
+                />
+            </Head>
 
-  let loadTodos = async () => {
-    const { data } = await axios.get("/api/list");
-    setTodos(data);
-    setLoading(false);
-  };
+            <main className={styles.main}>
+                <div className={styles.grid}>
+                    <h1 className={styles.title}>
+                        TODO 📃 App with{" "}
+                        <a href="https://blog.upstash.com/nextjs-todo">
+                            Next.js!
+                        </a>
+                        <br />
+                        <br />
+                    </h1>
 
-  if (!todos) {
-    return <div data-testid="loading">Loading...</div>;
-  }
+                    <form className={styles.cardForm} onSubmit={addTodo}>
+                        <input
+                            value={todo}
+                            data-testid="todo-input"
+                            className={styles.cardInput}
+                            type="text"
+                            name="todo"
+                            onChange={changeHandler}
+                            placeholder="What are you going to TODO?"
+                        />
+                    </form>
 
-  return (
-    <div className={styles.container} data-testid="application">
-      <Head>
-        <title>A5 - TODO</title>
-        <link
-          rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨‍🏫</text></svg>"
-        />
-      </Head>
+                    {todos.map((item) => (
+                        <a
+                            data-testid="todo-item"
+                            key={item.id}
+                            href="#"
+                            onClick={() => removeTodo(item)}
+                            className={styles.card}
+                        >
+                            <p>{item.title}</p>
+                        </a>
+                    ))}
+                </div>
+            </main>
 
-      <main className={styles.main}>
-        <div className={styles.grid}>
-          <h1 className={styles.title}>
-            TODO 📃 App with{" "}
-            <a href="https://blog.upstash.com/nextjs-todo">Next.js!</a>
-            <br />
-            <br />
-          </h1>
-          {loading ? (
-            <a href="#" className={styles.card}>
-              <Image width={20} height={20} alt="loading" src="/loader.gif" />
-            </a>
-          ) : (
-            <form className={styles.cardForm} onSubmit={addTodo}>
-              <input
-                data-testid="todo-input"
-                className={styles.cardInput}
-                type="text"
-                name="todo"
-                onChange={changeHandler}
-                placeholder="What are you going to TODO?"
-              />
-            </form>
-          )}
-          {todos.map((item) => (
-            <a
-              data-testid="todo-item"
-              key={item.id}
-              href="#"
-              onClick={() => removeTodo(item)}
-              className={styles.card}
-            >
-              <p>{item.title}</p>
-            </a>
-          ))}
+            <footer className={styles.footer}>
+                <a
+                    href="https://blog.upstash.com/nextjs-todo"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Powered by 👩‍🏫
+                </a>
+            </footer>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://blog.upstash.com/nextjs-todo"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by 👩‍🏫
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
